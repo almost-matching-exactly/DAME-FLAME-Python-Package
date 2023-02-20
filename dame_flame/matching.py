fixed off by one errors
Iterations start at 0. An iteration is completed once we have successfully attempted the match for that iteration. Also fixed pe_stopping and adjusted defaults for pre_dame and early_stop_iterations to be float('inf')

dame_flame/matching.py
@@ -69,7 +69,7 @@ class MatchParent:
         iteration.
     """
     def __init__(self, adaptive_weights='ridge', alpha=0.1, repeats=True,
-                 verbose=2, early_stop_iterations=False,
+                 verbose=2, early_stop_iterations=float('inf'),
                  stop_unmatched_c=False, early_stop_un_c_frac=False,
                  stop_unmatched_t=False, early_stop_un_t_frac=False,
                  early_stop_pe=True, early_stop_pe_frac=0.05,
@@ -166,9 +166,9 @@ class DAME(MatchParent):
             self.groups_per_unit = self.df_units_and_covars_matched['weights']
             self.df_units_and_covars_matched = self.df_units_and_covars_matched.drop(columns = ['weights'])
             self.units_per_group = return_array[1]
-            if (self.want_pe == True):
+            if self.want_pe:
                 self.pe_each_iter = return_array[2]
-            if (self.want_bf == True):
+            if self.want_bf:
                 self.bf_each_iter = return_array[-1]
         else:
             # in the mice case:
@@ -190,7 +190,7 @@ class FLAME(MatchParent):
     '''
     The class used for the FLAME algorithm
     '''
-    def predict(self, input_data, pre_dame=False, C=0.1):
+    def predict(self, input_data, pre_dame=float('inf'), C=0.1):
         """
         Performs match and returns matched data.
 
@@ -199,8 +199,8 @@ class FLAME(MatchParent):
         input_data: {string, dataframe}, required parameter
             The dataframe on which to perform the matching, or the location of
             the CSV with the dataframe
-        pre_dame (int, False): Indicates whether to switch to dame and after
-            int number of iterations.
+        pre_dame (int, float): Indicates whether to switch to dame after
+            int number of iterations. If float('inf') (default), only run FLAME.
         C (float, 0.1): The tradeoff between PE and BF in computing MQ
         """
         self.input_data, self.holdout_data = data_cleaning.read_files(
@@ -222,7 +222,7 @@ class FLAME(MatchParent):
         self.pe_each_iter = None
 		
         # in the non-mice case:
-        if self.missing_data_replace != 3 and not pre_dame:
+        if self.missing_data_replace != 3 and pre_dame == float('inf'):
             self.df_units_and_covars_matched = return_array[0]
             self.groups_per_unit = self.df_units_and_covars_matched['weights']
             self.df_units_and_covars_matched = self.df_units_and_covars_matched.drop(columns=['weights'])
@@ -232,7 +232,7 @@ class FLAME(MatchParent):
             if self.want_bf:
                 self.bf_each_iter = return_array[-1]
 
-        if (self.missing_data_replace != 3 and pre_dame==False):
+        if (self.missing_data_replace != 3 and pre_dame == float('inf')):
             self.df_units_and_covars_matched = return_array[0]
             self.groups_per_unit = self.df_units_and_covars_matched['weights']
             self.df_units_and_covars_matched = self.df_units_and_covars_matched.drop(columns = ['weights'])
@@ -242,7 +242,7 @@ class FLAME(MatchParent):
             if (self.want_bf == True):
                 self.bf_each_iter = return_array[-1]
 
-        elif pre_dame:
+        elif pre_dame < float('inf'):
 
             # the first few items all look the same, then the last item is from dame
             self.df_units_and_covars_matched = return_array[0]
@@ -300,7 +300,7 @@ class FLAME(MatchParent):
 def _DAME(df, df_holdout, treatment_column_name='treated', weight_array=False,
           outcome_column_name='outcome', adaptive_weights='ridge', alpha=0.1,
           repeats=True, verbose=2, want_pe=False,
-          early_stop_iterations=False, stop_unmatched_c=False,
+          early_stop_iterations=float('inf'), stop_unmatched_c=False,
           early_stop_un_c_frac=False, stop_unmatched_t=False,
           early_stop_un_t_frac=False, early_stop_pe=True,
           early_stop_pe_frac=0.05, want_bf=False, missing_indicator=np.nan,
@@ -369,22 +369,22 @@ def _DAME(df, df_holdout, treatment_column_name='treated', weight_array=False,
 def _FLAME(df, df_holdout, treatment_column_name='treated', weight_array=False,
            outcome_column_name='outcome', adaptive_weights='ridge', alpha=0.1,
            repeats=True, verbose=2, want_pe=False,
-           early_stop_iterations=False, stop_unmatched_c=False,
+           early_stop_iterations=float('inf'), stop_unmatched_c=False,
            early_stop_un_c_frac=False, stop_unmatched_t=False,
            early_stop_un_t_frac=False, early_stop_pe=True,
            early_stop_pe_frac=0.05, want_bf=False,
            missing_indicator=np.nan,
            missing_data_replace=0, missing_holdout_replace=0,
            missing_holdout_imputations=10, missing_data_imputations=0,
-           pre_dame=False, C=0.1):
+           pre_dame=float('inf'), C=0.1):
     """ This function kicks off the FLAME algorithm.
 
     Args:
         See DAME above. The exeption is no weight_array, and the additional
         params below:
 
-        pre_dame (int, False): Indicates whether to switch to dame and after
-            int number of iterations.
+        pre_dame (int, float): Indicates whether to switch to dame and after
+            int number of iterations. A value of float('inf') (default) means only FLAME is run.
         C (float, 0.1): The tradeoff between PE and BF in computing MQ
 
 
@@ -418,7 +418,7 @@ def _FLAME(df, df_holdout, treatment_column_name='treated', weight_array=False,
     else:
         # this would mean we need to run mice on the matching data, which means
         # that we have to run flame_generic multiple times
-        if pre_dame:
+        if pre_dame < float('inf'):
             raise Exception("Invalid Input Error. At this time, we do not "\
                             "allow users to run the hybrid algorithm while "\
                             "running MICE on the matching dataset.")