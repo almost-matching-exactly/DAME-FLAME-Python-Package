Parallelization and fixing overflow issue

dame_flame/flame_algorithm.py
@@ -7,18 +7,82 @@
 
 import pandas as pd
 import numpy as np
-
 from . import grouped_mr
 from . import dame_algorithm
 from . import flame_dame_helpers
+from . import shared_memory
+import joblib
+from joblib import Parallel, delayed, parallel_backend
+import pickle
+import time
+import os
+
+# Helper function only for n_jobs>1 (multiprocessing)
+def possible_drop(poss_drop, all_covs, consider_dropping, prev_drop, df_all,
+                treatment_column_name, outcome_column_name, df_holdout_array_shared,
+                adaptive_weights, alpha_given, df_unmatched_shared, return_matches,
+                C, weight_array, return_all=True ):
+    # S is the set of covars we drop. We try dropping each one
+    start = time.time()
+    
+    df_holdout_array = [df_holdout_array_shared[i].read() for i in range(len(df_holdout_array_shared))]
+    df_all_temp = df_all.read().copy(deep=True)
+    df_unmatched = df_unmatched_shared.read().copy(deep=True)
+    # return_matches_save = return_matches.read().copy(deep=True).astype('Int64')
+    # return_matches_temp = return_matches_save.replace(-100, '*')
+    
+    with open(return_matches, 'rb') as handle:
+        return_matches_temp = pickle.load(handle)
+
+    
+    s = prev_drop.union([poss_drop])
+
+    PE = flame_dame_helpers.find_pe_for_covar_set(
+    df_holdout_array, treatment_column_name, outcome_column_name, s,
+    adaptive_weights, alpha_given)
+
+    # error check. PE can be float(0), but not denote error
+    if not PE and type(PE) == bool:
+        return False, False, False, False, False
+
+    # The dropping criteria for FLAME is max MQ
+    # MQ = C * BF - PE
+
+    all_covs = set(all_covs)
+    covs_match_on = all_covs.difference([poss_drop]).difference(prev_drop)
+    covs_match_on = list(covs_match_on)
+    
+    matched_rows, return_matches_temp, units_in_g = grouped_mr.algo2_GroupedMR(
+        df_all_temp, df_unmatched, covs_match_on, all_covs,
+        treatment_column_name, outcome_column_name, return_matches_temp)
+    
+
+    # find the BF for this covariate set's match.
+    BF = flame_dame_helpers.compute_bf(
+        matched_rows, treatment_column_name, df_unmatched)
+
+    # Use the largest MQ as the covariate set to drop.
+    MQ = C * BF - PE
+    
+    if return_all:
+        result = {'mq': MQ, 'PE': PE, 'BF': BF, 'poss_drop': poss_drop, \
+              'return_matches_temp': return_matches_temp, 'matched_rows': matched_rows, 'units_in_g': units_in_g}
+    else:
+        result = {'mq': MQ, 'poss_drop': poss_drop}
+    return result
+
+def get_largest_mq(results):
+    start = time.time()
+    result =  max(results, key=lambda x:x['mq'])
+    print('get_largest_mq:', time.time() - start)
+    return result
 
 def decide_drop(all_covs, consider_dropping, prev_drop, df_all,
                 treatment_column_name, outcome_column_name, df_holdout_array,
                 adaptive_weights, alpha_given, df_unmatched, return_matches,
-                C, weight_array):
+                C, weight_array, n_cpus):
     """
     This is a helper function, where we decide which covar to drop next
-
     Args:
         all_covs (array): Array of covar column names, not including treatment
             and outcome columns.
@@ -26,8 +90,11 @@ def decide_drop(all_covs, consider_dropping, prev_drop, df_all,
             been dropped in a previous iteration
         prev_drop (set): Covariate column names that have been dropped
             in a previous iteration
-
-    """
+        n_cpus (int): Define the maximum number of CPUs to be used for parallelizing 
+            a for loop in the decide_drop function of the FLAME algorithm. The actual 
+            number of CPUs used will be min(n_cpus, cpu_count, number_dropping), where 
+            cpu_count is the number of CPUs found in your system, and number_dropping
+            is the number of covariates to be dropped in each iteration.     """
 
     # This is where we decide who to drop, and also compute the pe
     # value that gets outputted in the list described in readme.
@@ -38,7 +105,27 @@ def decide_drop(all_covs, consider_dropping, prev_drop, df_all,
     best_bf = 0
     best_pe = 0
     best_units_in_g = 0
+    
+    cpu_count = joblib.cpu_count()
+    n_jobs = min(n_cpus, cpu_count, len(consider_dropping))
 
+    '''
+    Copy df to shared memory for fast access, only for multiprocessing
+    '''
+    if n_jobs > 1:
+        df_all_shared = shared_memory.SharedPandasDataFrame(df_all)
+        df_holdout_array_shared = [shared_memory.SharedPandasDataFrame(df_holdout_array[i]) for i in range(len(df_holdout_array))]
+        df_unmatched_shared = shared_memory.SharedPandasDataFrame(df_unmatched)
+        # Need to convert to float64 so the dtype of the DataFrame is not 'object' 
+        # If the DataFrame is an object and we copy it to shared memory, there will be a segment fault error since an object 
+        # DataFrame is passing by reference, and other processes cannot access the correct values
+        # return_matches_save = return_matches.copy(deep=True).replace('*', -100).astype('float64')
+        
+        with open('./return_matches.pkl', 'wb') as handle:
+            pickle.dump(return_matches, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        return_matches_shared = './return_matches.pkl'
+
+        
     if not adaptive_weights:
         # find the covariate that can be dropped with the minimum value in
         # the weight array
@@ -56,66 +143,97 @@ def decide_drop(all_covs, consider_dropping, prev_drop, df_all,
         covs_match_on = list(covs_match_on)
 
         # need to make sure we don't edit the mutable dataframes, then do match
+        
         df_all_temp = df_all.copy(deep=True)
-        return_matches_temp = return_matches.copy(deep=True)
+        return_matches_temp1 = return_matches.copy(deep=True)
+        
         matched_rows, return_matches, units_in_g = grouped_mr.algo2_GroupedMR(
             df_all_temp, df_unmatched, covs_match_on, all_covs,
-            treatment_column_name, outcome_column_name, return_matches_temp)
+            treatment_column_name, outcome_column_name, return_matches_temp1)
 
         # find the BF for this covariate set's match.
         BF = flame_dame_helpers.compute_bf(matched_rows,
                                            treatment_column_name, df_unmatched)
-
         return best_drop, 0, matched_rows, return_matches, BF, units_in_g
-
     else:
-        for poss_drop in consider_dropping:
-            # S is the set of covars we drop. We try dropping each one
-            s = prev_drop.union([poss_drop])
+        #timing the decide_drop function
+        results = []
+        print(f"Consider dropping: {len(consider_dropping)}. Using {n_jobs} CPUs")
+        
+        if n_jobs == 1:
+            for poss_drop in consider_dropping:
+                # S is the set of covars we drop. We try dropping each one
+                s = prev_drop.union([poss_drop])
 
-            PE = flame_dame_helpers.find_pe_for_covar_set(
-                df_holdout_array, treatment_column_name, outcome_column_name, s,
-                adaptive_weights, alpha_given)
+                PE = flame_dame_helpers.find_pe_for_covar_set(
+                    df_holdout_array, treatment_column_name, outcome_column_name, s,
+                    adaptive_weights, alpha_given)
 
-            # error check. PE can be float(0), but not denote error
-            if not PE and type(PE) == bool:
-                return False, False, False, False, False
+                # error check. PE can be float(0), but not denote error
+                if not PE and type(PE) == bool:
+                    return False, False, False, False, False
 
-            # The dropping criteria for FLAME is max MQ
-            # MQ = C * BF - PE
+                # The dropping criteria for FLAME is max MQ
+                # MQ = C * BF - PE
 
-            all_covs = set(all_covs)
-            covs_match_on = all_covs.difference([poss_drop]).difference(prev_drop)
-            covs_match_on = list(covs_match_on)
+                all_covs = set(all_covs)
+                covs_match_on = all_covs.difference([poss_drop]).difference(prev_drop)
+                covs_match_on = list(covs_match_on)
 
-            # need to make sure we don't edit the mutable dataframes, then do match
-            df_all_temp = df_all.copy(deep=True)
-            return_matches_temp = return_matches.copy(deep=True)
-            matched_rows, return_matches_temp, units_in_g = grouped_mr.algo2_GroupedMR(
-                df_all_temp, df_unmatched, covs_match_on, all_covs,
-                treatment_column_name, outcome_column_name, return_matches_temp)
+                # need to make sure we don't edit the mutable dataframes, then do match
+                df_all_temp = df_all.copy(deep=True)
+                return_matches_temp = return_matches.copy(deep=True)
+                matched_rows, return_matches_temp, units_in_g = grouped_mr.algo2_GroupedMR(
+                    df_all_temp, df_unmatched, covs_match_on, all_covs,
+                    treatment_column_name, outcome_column_name, return_matches_temp)
 
-            # find the BF for this covariate set's match.
-            BF = flame_dame_helpers.compute_bf(
-                matched_rows, treatment_column_name, df_unmatched)
+                # find the BF for this covariate set's match.
+                BF = flame_dame_helpers.compute_bf(
+                    matched_rows, treatment_column_name, df_unmatched)
 
-            # Use the largest MQ as the covariate set to drop.
-            MQ = C * BF - PE
-            if MQ > best_mq:
-                best_mq = MQ
-                best_pe = PE
-                best_bf = BF
-                best_drop = poss_drop
-                best_return_matches = return_matches_temp
-                best_matched_rows = matched_rows
-                best_units_in_g = units_in_g
+                # Use the largest MQ as the covariate set to drop.
+                MQ = C * BF - PE
+                if MQ > best_mq:
+                    best_mq = MQ
+                    best_pe = PE
+                    best_bf = BF
+                    best_drop = poss_drop
+                    best_return_matches = return_matches_temp
+                    best_matched_rows = matched_rows
+                    best_units_in_g = units_in_g
+                
+        else:
+            results_reduced = Parallel(n_jobs=n_jobs, verbose=2)(\
+                delayed(possible_drop)(poss_drop, all_covs, consider_dropping, prev_drop, df_all_shared,\
+                treatment_column_name, outcome_column_name, df_holdout_array_shared,\
+                adaptive_weights, alpha_given, df_unmatched_shared, return_matches_shared,\
+                C, weight_array,return_all=False ) for poss_drop in consider_dropping)
+            max_mq = get_largest_mq(results_reduced)
+            result_max = possible_drop(max_mq['poss_drop'], all_covs, consider_dropping, prev_drop, df_all_shared,\
+                treatment_column_name, outcome_column_name, df_holdout_array_shared,\
+                adaptive_weights, alpha_given, df_unmatched_shared, return_matches_shared,\
+                C, weight_array, return_all=True )
+
+            best_mq = result_max['mq']
+            best_pe = result_max['PE']
+            best_bf = result_max['BF']
+            best_drop = result_max['poss_drop']
+            best_return_matches = result_max['return_matches_temp']
+            best_matched_rows = result_max['matched_rows']
+            best_units_in_g = result_max['units_in_g']
+        
+            df_all_shared.unlink()
+            [obj.unlink() for obj in df_holdout_array_shared]
+            df_unmatched_shared.unlink()
+            # return_matches_shared.unlink()
+            os.remove(return_matches_shared)
 
         return best_drop, best_pe, best_matched_rows, best_return_matches, best_bf, best_units_in_g
 
 def flame_generic(df_all, treatment_column_name, weight_array,
                   outcome_column_name, adaptive_weights, alpha, df_holdout,
                   repeats, want_pe, verbose, want_bf, missing_holdout_replace,
-                  early_stops, pre_dame, C):
+                  early_stops, pre_dame, C, n_cpus):
     '''
     All variables are the same as dame algorithm 1 except for:
     pre_dame(False, integer): Indicates whether the algorithm will move to
@@ -147,9 +265,9 @@ def flame_generic(df_all, treatment_column_name, weight_array,
 						
     covs_match_on = all_covs
     matched_rows, return_matches, units_in_g = grouped_mr.algo2_GroupedMR(
-        df_all, df_unmatched, covs_match_on, all_covs, treatment_column_name,
-        outcome_column_name, return_matches)
-
+        df_all, df_unmatched, covs_match_on, all_covs,
+        treatment_column_name, outcome_column_name, return_matches)
+        
     if (len(units_in_g)) != 0:
         bf = flame_dame_helpers.compute_bf(matched_rows, treatment_column_name, df_unmatched)
 
@@ -159,8 +277,7 @@ def flame_generic(df_all, treatment_column_name, weight_array,
         # flatten to 1 list, then add occurrences of unique units
         flat_units_in_g = np.concatenate(units_in_g).ravel()
         unique_units, occurrences = np.unique(flat_units_in_g, return_counts=True)
-        for index in range(len(unique_units)):
-            weights['weights'][unique_units[index]] += occurrences[index]
+        weights.loc[unique_units,'weights'] += occurrences
     else:
         bf = 0
 
@@ -219,7 +336,7 @@ def flame_generic(df_all, treatment_column_name, weight_array,
         new_drop, pe, matched_rows, return_matches, bf, units_in_g = decide_drop(all_covs,
             consider_dropping, prev_dropped, df_all, treatment_column_name,
             outcome_column_name, df_holdout_array, adaptive_weights, alpha,
-            df_unmatched, return_matches, C, weight_array)
+            df_unmatched, return_matches, C, weight_array, n_cpus)
 
         # Check for error in above step:
         if not new_drop:
@@ -227,7 +344,6 @@ def flame_generic(df_all, treatment_column_name, weight_array,
                             "machine learning algorithm used to choose the "\
                             "covariate to drop. For help, please reach on "\
                             "github to the team. ")
-
         if (len(units_in_g)) != 0:
         # add the newly matched groups to MG_units, which tracks units in groups
             MG_units = MG_units + units_in_g
@@ -235,8 +351,7 @@ def flame_generic(df_all, treatment_column_name, weight_array,
             # flatten to 1 list, then add occurrences of unique units
             flat_units_in_g = np.concatenate(units_in_g).ravel()
             unique_units, occurrences = np.unique(flat_units_in_g, return_counts=True)
-            for index in range(len(unique_units)):
-                weights['weights'][unique_units[index]] += occurrences[index]
+            weights.loc[unique_units,'weights'] += occurrences
 
         # Check not equal to false because if it's turned off, value is False
         baseline_pe = max(1e-12, baseline_pe)
@@ -271,7 +386,6 @@ def flame_generic(df_all, treatment_column_name, weight_array,
 
             if want_bf:
                 print("\tBalancing Factor of this iteration: ", bf)
-
         # Do we switch to DAME?
         if (pre_dame and pre_dame <= h):
 
@@ -292,6 +406,7 @@ def flame_generic(df_all, treatment_column_name, weight_array,
 
             # when dame is done, we
             # return the matches we made here, plus the matches made in dame.
+            
 
             # but first, make sure anything not matched isn't in the df:
             return_matches = return_matches.dropna(axis=0) #drop rows with nan
@@ -303,7 +418,6 @@ def flame_generic(df_all, treatment_column_name, weight_array,
                 return_package.append(return_bf)
             return_package.append(return_matches_dame)
             return return_package
-
 
         # end loop.
 

dame_flame/flame_group_by.py
@@ -6,6 +6,11 @@
 # License: MIT
 
 import numpy as np
+import math
+import decimal
+
+def cal_dec(x):
+    return math.ceil(np.log10(x))
 
 def match_ng(df, covs, covs_max_list, treatment_indicator_col='treated'):
     '''
@@ -22,11 +27,23 @@ def match_ng(df, covs, covs_max_list, treatment_indicator_col='treated'):
     # the covariate values together with the treatment indicator as a matrix
     arr_slice_w_t = df[covs + [treatment_indicator_col]].values
 
+    # calculate required precision
+    precision1=cal_dec(arr_slice_wo_t.max())+cal_dec(arr_slice_wo_t.shape[1])+ math.ceil(np.max(np.log10(covs_max_list)*np.arange(len(covs_max_list))))
+    precision2=cal_dec(arr_slice_w_t.max())+cal_dec(arr_slice_w_t.shape[1])+ math.ceil(np.max(np.log10(covs_max_list)*np.arange(1,len(covs_max_list)+1)))
+    decimal.getcontext().prec=max(precision1,precision2)
     # matrix multiplication, get a unique number for each unit
-    b_i = np.dot(arr_slice_wo_t, np.power(covs_max_list, [i for i in range(len(covs_max_list))]))
+    if max(precision1,precision2)<=18:
+        dtype=np.int64
+    else:
+        dtype=decimal.Decimal
+    b_i = np.dot(arr_slice_wo_t, np.power(np.array(covs_max_list).astype(dtype), np.arange(len(covs_max_list))))
+
+    # b_i = np.dot(arr_slice_wo_t, np.power(covs_max_list, [i for i in range(len(covs_max_list))]))
 
     # matrix multiplication, get a unique number for each unit with treatment indicator
-    b_i_plus = np.dot(arr_slice_w_t, np.append(np.power(covs_max_list, [i+1 for i in range(len(covs_max_list))]), [1]))
+    b_i_plus = np.dot(arr_slice_w_t, np.append(np.power(np.array(covs_max_list).astype(dtype), np.arange(1,len(covs_max_list)+1)), [1]))
+
+    # b_i_plus = np.dot(arr_slice_w_t, np.append(np.power(covs_max_list, [i+1 for i in range(len(covs_max_list))]), [1]))
     # b_i_plus = np.dot(arr_slice_w_t, np.array([covs_max_list[i]**(i+1) for i in range(len(covs_max_list))] + [1]))
 
     # count how many times each number appears